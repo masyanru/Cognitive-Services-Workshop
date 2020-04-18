e8dd03ddfa9ab77193ed4a6ed6d1ac4e383f605a
Anton Mosyagin <anton@masyan.ru>
2020-04-18 17:54:35 +0300

    Translator Text

--- qnamaker.py
+++ qnamaker.py
@@ -3,13 +3,13 @@ import requests
 from pprint import pprint
 import os
 
-subscription_key = ""
+subscription_key = "EndpointKey 61c28b98-abd7-4db7-a88e-0f7ee2645cd7"
 endpoint = "https://qnamakerworkshopmai.azurewebsites.net/qnamaker"
 
 # language detection
 language_api_url = endpoint + "/knowledgebases/bbc18410-efc4-4756-b122-0bb87e2a08cb/generateAnswer"
 
-question = {"question": "привет"}
+question = {"question": "как добраться"}
 
 headers = {"Authorization": subscription_key}
 response = requests.post(language_api_url, headers=headers, json=question)
---
+++ translate.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import os, requests, uuid, json
+
+subscription_key = ''
+endpoint = 'https://api.cognitive.microsofttranslator.com'
+
+# that you are using the latest endpoint: https://docs.microsoft.com/azure/cognitive-services/translator/reference/v3-0-translate
+path = '/translate?api-version=3.0'
+params = '&to=en&to=ru'
+constructed_url = endpoint + path + params
+
+headers = {
+    'Ocp-Apim-Subscription-Key': subscription_key,
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+}
+
+# You can pass more than one object in body.
+body = [{
+    'text': 'Hello World!'
+}]
+request = requests.post(constructed_url, headers=headers, json=body)
+response = request.json()
+
+print(json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False))
